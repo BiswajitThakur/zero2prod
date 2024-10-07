3fc49d937625cb61e1cea1b25c8d1153ddcc9da7
BiswajitThakur <biswajit733127@gmail.com>
2024-10-07 20:17:26 +0530

    _

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+//! main.rs
+
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    println!("Server running: 127.0.0.1:{port}");
+    zero2prod::run(listener)
+        .expect("Failed to bind address")
+        .await
 }
